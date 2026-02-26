cb540cc81514e2b81062bffd712602a7b009f156
youngtaek_hong <youngtaek_hong@dweax.com>
2026-02-26 11:13:24 +0900

    Feat: 환경변수 설정 추가

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -1,13 +1,21 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const port = Number(env.VITE_PORT ?? '3000')
+
+  return {
+    plugins: [
+      react({
+        babel: {
+          plugins: [['babel-plugin-react-compiler']],
+        },
+      }),
+    ],
+    server: {
+      port,
+    },
+  }
 })
